6c4db0367397ab021a00998008146cd83ef7e6f6
Rajshekhar <ursvraj@gmail.com>
2020-02-23 11:27:50 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-const mysqlConnection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'vasu_shop'
-});
+// const mysqlConnection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'root',
+//     database: 'vasu_shop'
+// });
 
-mysqlConnection.connect((err)=>{
-    if(!err) return console.log('DB connection succeded')
-    console.log('DB connection failed', JSON.stringify(err, null, 2))
-})
+// mysqlConnection.connect((err)=>{
+//     if(!err) return console.log('DB connection succeded')
+//     console.log('DB connection failed', JSON.stringify(err, null, 2))
+// })
 
 const port = process.env.port || 3002;
 
